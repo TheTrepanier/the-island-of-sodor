c977e8323d1471082ab8e0425a1865e194aa733b
TheTrepanier <thetrepanier1@Michaels-MacBook-Pro-3.local>
2019-04-02 16:23:06 -0700

    Basic functionality completed, ready to submit

--- assets/js/tankengine.js
+++ assets/js/tankengine.js
@@ -1,22 +1,21 @@
+var rightNow = moment().unix();
+
 $("#submit-button").on("click", function() {
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
-    var firstTrainTime = $("#first-train-time").val().trim();
+    var firstTrainTimeString = $("#first-train-time").val().trim();
+    var firstTrainTime = moment(firstTrainTimeString, "h:mm");
+    var firstTrainTimeUnix = moment(firstTrainTime).format("X");
     var frequency = $("#frequency").val().trim();
 
     var newTrain = {
         name: trainName,
         destination: destination,
-        firstTrainTime: firstTrainTime,
+        firstTrainTime: firstTrainTimeUnix,
         frequency: frequency
     }
 
     database.ref().push(newTrain);
-
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTrainTime);
-    console.log(newTrain.frequency);
 
     $("#train-name").val("");
     $("#destination").val("");
@@ -25,5 +24,34 @@ $("#submit-button").on("click", function() {
 });
 
 database.ref().on("child_added", function(childSnapshot) {
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var firstTrainTime = Number(childSnapshot.val().firstTrainTime);
+    var frequency = childSnapshot.val().frequency;
+    
+    var firstTrainTimeReadable = moment.unix(firstTrainTime).format("HH:mm");
+    var nextTrainTime = firstTrainTime;
 
+    while (nextTrainTime < rightNow) {
+        if (nextTrainTime > rightNow) {
+            break;
+        }
+        nextTrainTime += (frequency * 60);        
+    }
+
+
+    var nextTrainTimeReadable = moment.unix(nextTrainTime).format("HH:mm");
+
+    console.log(firstTrainTime);
+    console.log(firstTrainTimeReadable);
+    console.log(nextTrainTimeReadable);
+    
+
+    var trainNameCol = $("<td>").text(trainName);
+    var destinationCol = $("<td>").text(destination);
+    var frequencyCol = $("<td>").text(frequency);
+    var nextArrivalCol = $("<td>").text(nextTrainTimeReadable);
+    var MinutesAwayCol = $("<td>").text(Math.round((nextTrainTime - rightNow) / 60));
+    var row = $("<tr>").append(trainNameCol, destinationCol, frequencyCol, nextArrivalCol, MinutesAwayCol);
+    $("tbody").append(row);
 });
